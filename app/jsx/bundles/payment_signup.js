f7e2a33da392c983919b6da1fb33889994ab1940
Andrew Gauger <andygauge@gmail.com>
2020-03-15 20:36:22 -0700

    javascript lint

--- app/jsx/bundles/payment_signup.js
+++ app/jsx/bundles/payment_signup.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Elements} from '@stripe/react-stripe-js';
-import {loadStripe} from '@stripe/stripe-js';
+/*
+ * Copyright (C) 2020 - present Instructure, Inc.
+ *
+ * This file is part of Canvas.
+ *
+ * Canvas is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, version 3 of the License.
+ *
+ * Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-import PaymentSignup from '../on_guard/payment/sign_up_form';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Elements} from '@stripe/react-stripe-js'
+import {loadStripe} from '@stripe/stripe-js'
 
-const stripePromise = loadStripe("pk_test_u9GdTvcNq92kCmp7khlDrnRz007AyPREka");
+import PaymentSignup from '../on_guard/payment/sign_up_form'
+
+const stripePromise = loadStripe('pk_test_u9GdTvcNq92kCmp7khlDrnRz007AyPREka')
 
 function App() {
   return (
     <Elements stripe={stripePromise}>
       <PaymentSignup />
     </Elements>
-  );
-};
+  )
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
