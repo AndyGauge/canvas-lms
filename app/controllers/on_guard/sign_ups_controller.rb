0ae6ad43b5e0d031be921b6b7c9c67c77f9af105
Andrew Gauger <andygauge@gmail.com>
2020-04-29 21:17:06 -0700

    https://kanbanflow.com/t/pPY9gMti On sign up receive invitation

--- app/controllers/on_guard/sign_ups_controller.rb
+++ app/controllers/on_guard/sign_ups_controller.rb
@@ -19,7 +19,7 @@ module OnGuard
     end
 
     def create
-      @sign_up = OnGuard::SignUp.new(params).create
+      @sign_up = OnGuard::SignUp.new(params, load_account).create
       successful_login(@sign_up.user, @sign_up.pseudonym, false, false)
       render json: {status: @sign_up.success, user_id: @sign_up.user.id}
     end
--- app/helpers/on_guard/sign_up.rb
+++ app/helpers/on_guard/sign_up.rb
@@ -1,8 +1,9 @@
 module OnGuard
   class SignUp
     attr_reader :user, :complete, :pseudonym, :success
-    def initialize(params)
+    def initialize(params, domain_user_account = nil )
       @params = params
+      @domain_user_account = domain_user_account
     end
     def create
       cc_addr =@params[:user][:email]
@@ -13,8 +14,9 @@ module OnGuard
         @user.workflow_state='registered'
         @pseudonym = @user.create_pseudonym(unique_id: cc_addr, password: @params[:password], password_confirmation: @params[:passwordConfirmation], workflow_state: 'active')
         @user.accept_terms
-        @user.communication_channels.build(:path_type => CommunicationChannel::TYPE_EMAIL, :path => cc_addr)
+        cc = @user.communication_channels.build(:path_type => CommunicationChannel::TYPE_EMAIL, :path => cc_addr)
         @user.save!
+        cc.send_confirmation!(@domain_user_account)
         PseudonymSession.new(@pseudonym).save unless @pseudonym.new_record?
         AccountUser.create user: @user, account: organization.account, role_id: 3  #StudentEnrollment role
         @success = OnGuard::Payment.new(organization).create_customer(@params[:paymentMethod])
