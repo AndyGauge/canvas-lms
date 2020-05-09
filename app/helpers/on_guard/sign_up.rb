fa4be6d1d00dc21e555c55ce82ee848281c01de4
Andrew Gauger <andygauge@gmail.com>
2020-05-09 12:37:25 -0700

    https://kanbanflow.com/t/pQqoXe56 on registration enroll in course

--- app/helpers/on_guard/sign_up.rb
+++ app/helpers/on_guard/sign_up.rb
@@ -7,23 +7,28 @@ module OnGuard
     end
     def create
       cc_addr =@params[:user][:email]
-      if EmailAddressValidator.valid?(cc_addr)
-        @user = User.create!(@params.require( :user ).permit( :name ))
-        organization = @user.create_on_guard_organization! name: @params[:organization]
-        @user.create_on_guard_supervisor! on_guard_organization: organization
-        @user.workflow_state='registered'
-        @pseudonym = @user.create_pseudonym(unique_id: cc_addr, password: @params[:password], password_confirmation: @params[:passwordConfirmation], workflow_state: 'active')
-        @user.accept_terms
-        cc = @user.communication_channels.build(:path_type => CommunicationChannel::TYPE_EMAIL, :path => cc_addr)
-        @user.save!
-        cc.send_confirmation!(@domain_user_account)
-        PseudonymSession.new(@pseudonym).save unless @pseudonym.new_record?
-        AccountUser.create user: @user, account: organization.account, role_id: 3  #StudentEnrollment role
-        @success = OnGuard::Payment.new(organization).create_customer(@params[:paymentMethod])
-        return self
-      else
-        raise 'Invalid Email'
+      raise 'Invalid Email' unless EmailAddressValidator.valid?(cc_addr)
+
+      @user = User.create!(@params.require( :user ).permit( :name ))
+      organization = @user.create_on_guard_organization! name: @params[:organization]
+      @user.create_on_guard_supervisor! on_guard_organization: organization
+      @user.workflow_state='registered'
+      @pseudonym = @user.create_pseudonym(unique_id: cc_addr, password: @params[:password], password_confirmation: @params[:passwordConfirmation], workflow_state: 'active')
+      @user.accept_terms
+
+      cc = @user.communication_channels.build(:path_type => CommunicationChannel::TYPE_EMAIL, :path => cc_addr)
+      @user.save!
+      cc.send_confirmation!(@domain_user_account)
+      PseudonymSession.new(@pseudonym).save unless @pseudonym.new_record?
+      AccountUser.create user: @user, account: organization.account, role_id: 3  #StudentEnrollment role
+
+      Course.published.where(auto_assigned: true).each do |c|
+        StudentEnrollment.create(course: c, user: @user)
       end
+
+      @success = OnGuard::Payment.new(organization).create_customer(@params[:paymentMethod])
+      return self
+
     end
     def update
       if @params[:billing]
